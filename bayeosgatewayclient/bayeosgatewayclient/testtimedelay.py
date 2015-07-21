daef27fc6036d51a33fd55b8da0e97317229eea6
Anja.Kleebaum@Kleebaum.de <anja.kleebaum@kleebaum.de>
2015-07-21 16:39:42 +0200

    improved testtimedelay script

--- bayeosgatewayclient/bayeosgatewayclient/testtimedelay.py
+++ bayeosgatewayclient/bayeosgatewayclient/testtimedelay.py
@@ -7,10 +7,6 @@ from bayeosgatewayclient import BayEOSWriter
 PATH = '/tmp/bayeos-device1/'
 writer = BayEOSWriter(PATH, 50)
 writer.save_msg('Writer was started.', origin='Python-Writer-Test')
-#print datetime.datetime.now().strftime("%S.%f")
-#print values
 while True:
-    values = []
-    values.append(float(datetime.datetime.now().strftime("%S.%f")))
-    writer.save(values, value_type=0x21, origin='Python-Writer-Test')
+    writer.save([float(datetime.datetime.now().strftime("%S.%f"))], value_type=0x21, origin='Python-Writer-Test')
     sleep(1)
