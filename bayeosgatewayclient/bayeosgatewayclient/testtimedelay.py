7cfdcdb3fdb1fd8fc66ec08372f2f455b83d271a
Anja.Kleebaum@Kleebaum.de <anja.kleebaum@kleebaum.de>
2015-07-22 17:02:18 +0200

    updated time delay test

--- bayeosgatewayclient/bayeosgatewayclient/testtimedelay.py
+++ bayeosgatewayclient/bayeosgatewayclient/testtimedelay.py
@@ -1,12 +1,20 @@
 """Script to test the time delay during writing."""
 
-from time import time, sleep
-import datetime
-from bayeosgatewayclient import BayEOSWriter
+from time import time, sleep, strptime, strftime, mktime
+from bayeosgatewayclient import BayEOSWriter, BayEOSSender
+from thread import start_new_thread
 
-PATH = '/tmp/bayeos-device1/'
+PATH = '/tmp/bayeos-device2/'
+URL = 'http://bayconf.bayceer.uni-bayreuth.de/gateway/frame/saveFlat'
 writer = BayEOSWriter(PATH, 50)
-writer.save_msg('Writer was started.', origin='Python-Writer-Test')
+writer.save_msg('Writer was started.')
+
+sender = BayEOSSender(PATH, 'Python-Writer-Test-Anja', URL, 'import', 'import')
+start_new_thread(sender.run, (5,))
+
+today=mktime(strptime(strftime('%Y-%m-%d'),'%Y-%m-%d'))
+start=time()
 while True:
-    writer.save([float(datetime.datetime.now().strftime("%S.%f"))], value_type=0x21, origin='Python-Writer-Test')
+    t=time()
+    writer.save([t-start, t-today], value_type=0x21)
     sleep(1)
